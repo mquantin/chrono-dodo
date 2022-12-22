d6464237c087dc2abf7f3bae5e72c5d580d9c65f
benjamin-maine <benjamin.maine@gmail.com>
2022-12-22 15:54:37 +0100

    feat(typescript): alias
    - enable alias ex: ../ui/Compo => @/ui/Compo

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,4 +1,8 @@
 module.exports = {
+  "plugins": [
+    "@typescript-eslint",
+    "import"
+  ],
   extends: [
     // By extending from a plugin config, we can get recommended rules without having to add them manually.
     'eslint:recommended',
@@ -6,21 +10,23 @@ module.exports = {
     'plugin:import/recommended',
     'plugin:jsx-a11y/recommended',
     'plugin:@typescript-eslint/recommended',
-    // This disables the formatting rules in ESLint that Prettier is going to be responsible for handling.
-    // Make sure it's always the last config, so it gets the chance to override other configs.
     'eslint-config-prettier',
   ],
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    "project": "./tsconfig.json",
+    "tsconfigRootDir": "./"
+  },
   settings: {
     react: {
-      // Tells eslint-plugin-react to automatically detect the version of React to use.
       version: 'detect',
     },
-    // Tells eslint how to resolve imports
     'import/resolver': {
+      typescript: {},
       node: {
         paths: ['src'],
         extensions: ['.js', '.jsx', '.ts', '.tsx'],
-      },
+      }
     },
   },
   rules: {
